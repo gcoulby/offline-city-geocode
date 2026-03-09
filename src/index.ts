49d2b54751c5df00d3d3c7e39dcde2b60e31884e
Graham <gcoulby@gmail.com>
2026-03-09 23:07:18 +0000

    add export

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ export { OfflineCityGeocoder } from './lookup.js'
 export { buildIndex } from './build.js'
 export type { BucketedIndex, CityRecord, ReverseGeocodeResult } from './types.js'
 
-import fs from 'node:fs'
-import zlib from 'node:zlib'
-
-const raw = fs.readFileSync(new URL('../data/index.zip', import.meta.url))
-const index = JSON.parse(zlib.gunzipSync(raw).toString())
-
+import index from '../data/index.json' with { type: 'json' }
+import { BucketedIndex } from './types.js'
 import { OfflineCityGeocoder } from './lookup.js'
 
-export const geocoder = new OfflineCityGeocoder(index)
+const x = index as BucketedIndex
+export const geocoder = new OfflineCityGeocoder(x)
